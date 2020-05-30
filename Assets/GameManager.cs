46fabb8f11c39bd89e08ce36422dfe66150acef7
DESKTOP-JAGFNBP\tohko <tohko.tech1125@gmai.com>
2020-05-30 19:42:55 +0900

    GameManagerの編集

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Hoge()
+    {
+
     }
 }
